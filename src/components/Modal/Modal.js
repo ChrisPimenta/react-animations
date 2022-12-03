91242051a008f3d6ef95c28a24adbf6aebf2d494
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-12-03 19:33:58 +0100

    Use CSS transition rather than transition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,41 +1,65 @@
 import React from 'react';
-import { Transition } from "react-transition-group";
+import { Transition, CSSTransition } from "react-transition-group";
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
-    const getModalVisibilityClass = (transitionState) => {
-        let modalVisibilityClass = null;
+    // const getModalVisibilityClass = (transitionState) => {
+    //     let modalVisibilityClass = null;
 
-        switch (transitionState) {
-            case 'exiting':
-                modalVisibilityClass = 'modal--closed'
-                break;
-            case 'entering':
-                modalVisibilityClass = 'modal--opened'
-                break;
-        }
+    //     switch (transitionState) {
+    //         case 'exiting':
+    //             modalVisibilityClass = 'modal--closed'
+    //             break;
+    //         case 'entering':
+    //             modalVisibilityClass = 'modal--opened'
+    //             break;
+    //     }
 
-        return modalVisibilityClass;
-    }
+    //     return modalVisibilityClass;
+    // }
 
     return (
-        <Transition
-            in={props.show}
-            timeout={300}
+        // <Transition
+        //     in={props.show}
+        //     timeout={animationTiming}
+        //     mountOnEnter
+        //     unmountOnExit
+        // >
+        //     {
+        //         transitionState => {
+        //             return (
+        //                 <div className={`modal ${getModalVisibilityClass(transitionState)}`}>
+        //                     <h1>A Modal</h1>
+        //                     <button className="Button" onClick={props.closed}>Dismiss</button>
+        //                 </div>
+        //             )
+        //         }
+        //     }
+        // </Transition>
+
+        // This is the easiest way to use transitions! 
+        <CSSTransition
             mountOnEnter
             unmountOnExit
+            in={props.show}
+            timeout={animationTiming}
+            classNames={{
+                enter: '',
+                enterActive: 'modal--opened',
+                exit: '',
+                exitActive: 'modal--closed'
+            }}
         >
-            {
-                transitionState => {
-                    return (
-                        <div className={`modal ${getModalVisibilityClass(transitionState)}`}>
-                            <h1>A Modal</h1>
-                            <button className="Button" onClick={props.closed}>Dismiss</button>
-                        </div>
-                    )
-                }
-            }
-        </Transition>
+            <div className='modal'>
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+        </CSSTransition>
 
     );
 }

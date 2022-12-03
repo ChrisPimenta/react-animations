debb3c51bdc2a9fdfd5cbbfec90574d76955ae17
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-12-03 19:43:16 +0100

    Animating lists with Transition Group

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// Used for Lists
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import './List.css';
 
@@ -23,21 +25,33 @@ class List extends Component {
         });
     }
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
+    render() {
+        const listItems = this.state.items.map((item, index) => (
+            <CSSTransition
                 key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+                classNames={{
+                    enter: 'fade--enter',
+                    enterActive: 'fade--enter-active',
+                    exit: 'fade--exit',
+                    exitActive: 'fade--exit-active'
+                }}
+                timeout={300}>
+                <li
+                    className="ListItem"
+                    onClick={() => this.removeItemHandler(index)}
+                >
+                    {item}
+                </li>
+            </CSSTransition>
+        ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup component="ul" className="List">
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }
